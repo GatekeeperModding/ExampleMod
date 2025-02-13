cc790b5bb007535db8c7159ee3ce1ca81b4a088e
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2025-02-13 01:04:16 +0100

    added examplemod

--- Plugin.cs
+++ Plugin.cs
@@ -1,18 +1,74 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
-using BepInEx.Unity.IL2CPP;
+using ExampleMod.Content.ItemControllers;
+using Gatekeeper.Items;
+using Gatekeeper.Utility;
+using GKAPI;
+using GKAPI.Achievements;
+using GKAPI.Difficulties;
+using GKAPI.Items;
+using GKAPI.Lang;
+using UnityEngine;
 
 namespace ExampleMod;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BasePlugin
+[BepInDependency(GKAPI.Plugin.PluginGuid)]
+public class Plugin : GkPlugin
 {
-    internal static new ManualLogSource Log;
+    internal new static ManualLogSource Log;
 
     public override void Load()
     {
+        base.Load();
         // Plugin startup logic
         Log = base.Log;
         Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    public override void AddContent()
+    {
+        if (EventHandler.State != EventHandler.LoadingState.PreInit)
+        {
+            Log.LogError("Content has to be added during Pre-Init!");
+            return;
+        }
+
+        GlobalSettings.Instance.buildCheating = true;
+        GlobalSettings.Instance.buildDebugConsole = true;
+
+        var achievementAPI = AchievementsAPI.Instance;
+        var baseAchievement = achievementAPI.AddAchievement(new GkAchievement.Builder());
+        
+        var itemAPI = ItemAPI.Instance;
+        var testItem = itemAPI.AddItem(new GkItem.Builder("Test Item", "Test item description", $"{ColorHelper.WrapInColor("{[Mod1_Lvl1]}% (+{[Mod1_Lvl2]}% per stack)", Colors.Red)} to critical damage.")
+            .WithId("TEST")
+            .SetUnlocked(true)
+            .SetHidden(false)
+            .AddModification(ItemParamModificationType.CritDamagePerc, 0.5f, 0.25f)
+        );
+        var bobItem = itemAPI.AddItem("Bob");
+        var testTriad = itemAPI.AddTriad("TestTriad", [ItemID.RuneOfRebound, ItemID.Triumph, testItem.GetItemID], builder => builder.SetUnlocked(true).SetHidden(false));
+        itemAPI.AddItemController<TestTriadItemController>(testTriad.GetItemID);
+        
+        baseAchievement.AddItems([testItem.Info, bobItem.Info]);
+
+        var diffAPI = DifficultiesAPI.Instance;
+        diffAPI.AddDifficulty(new GkDifficulty.Builder()
+            .WithName("Guardian")
+            .WithPercentageName("300%")
+            .WithDifficultyMultiplier(0.6f)
+            .WithPrismMultiplier(2f)
+            .WithEventsMinLevel(0)
+            .WithColors(new Color(0.1f, 0.1f, 0.5f), new Color(0.1f, 0.3f, 0.8f))
+        );
+        diffAPI.AddDifficulty(new GkDifficulty.Builder()
+            .WithName("Wtf is this")
+            .WithPercentageName("1000%")
+            .WithDifficultyMultiplier(2f)
+            .WithPrismMultiplier(4f)
+            .WithEventsMinLevel(0)
+            .WithColors(new Color(0.4f, 0.1f, 0.8f), new Color(0.4f, 0.3f, 0.8f))
+        );
     }
 }
